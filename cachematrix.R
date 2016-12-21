0a88270bec6abe5a84117db5bc4fe4be0fcfed53
Lulu Ma <ruruccu@126.com>
2016-12-21 13:05:35 +0800

    finish assignment 2 v1.0.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming - Programming Assignment 2: Lexical Scoping
+## Assignment: Caching the Inverse of a Matrix
+## This assignment contains two functions:
+## 1. makeCacheMatrix
+## 2. cacheSolve
 
-## Write a short comment describing this function
-
+## This function creates a special matrix that can cache its inverse, by doing:
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the inverse
+## 4. set the value of the inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv_x <- NULL
+    set <- function(y){
+        x <<- y
+        inv_x <<- NULL
+    }
+    get <- function(){
+        x 
+    }
+    setinverse <- function(inv_matrix) { 
+        inv_x <<- inv_matrix 
+    }
+    getinverse <- function() {
+        inv_x
+    }
+    
+    list(set = set, get = get,
+         setinverse = setinvers,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function calculates the inverse of the "makeCacheMatrix"
+## 1. if the inverse has already been calculated, it gets the inverse from cache
+## 2. if not, it calculates the inverse and set it in the cache
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv_mtx <- x$getinverse()
+    if(!is.null(inv_mtx)) {
+        message("getting inverse from cache")
+        return(inv_mtx)
+    }
+    org_mtx <- x$get()
+    inv_mtx <- solve(org_mtx,...)
+    x$setinverse(inv_mtx)
+    inv_mtx
 }
